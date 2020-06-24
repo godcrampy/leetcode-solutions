f3c59ca02e409804a90e8cf0072c41171bb4120b
Sahil <sahilbondre@gmail.com>
2020-06-25 00:03:47 +0530

    add q104 java, cpp, go

---
+++ 104-maximum-depth-of-binary-tree/go/main.go
@@ -0,0 +1,21 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := maxDepth(root.Left)
+	r := maxDepth(root.Right)
+	if l < r {
+		return r + 1
+	}
+	return l + 1
+}
